c0a6ec39afb9ab6c89c00864db57d2d5cd4776b1
hanvis <hanvis@MacBook-Pro-hanvis.local>
2024-09-27 20:27:46 +0400

    first page

--- script.js
+++ script.js
@@ -12,25 +12,39 @@ if (user) {
   const userPhotoElement = document.getElementById('userPhoto');
 
   // Заполняем информацию о пользователе
-  firstNameElement.textContent = `Имя: ${user.first_name}`;
-  lastNameElement.textContent = `Фамилия: ${user.last_name || 'не указана'}`;
-  userIdElement.textContent = `ID: ${user.id}`;
-  usernameElement.textContent = `Юзернейм: ${user.username || 'не указан'}`;
+  firstNameElement.textContent = user.first_name;
+  lastNameElement.textContent = user.last_name || '';
+  userIdElement.textContent = user.id;
+  // usernameElement.textContent = `Юзернейм: ${user.username || 'не указан'}`;
 
   // Проверяем наличие фотографии профиля
-  if (user.photo_url) {
-    // Загружаем изображение, чтобы проверить его доступность
-    const img = new Image();
-    
-    img.src = user.photo_url;
-    img.onload = function() {
-      userPhotoElement.src = user.photo_url;
-      userPhotoElement.style.display = 'block';
-    }
-    img.onerror = function() {
-      console.error('Не удалось загрузить изображение пользователя.');
-    }
-  }
+  // if (user.photo_url) {
+  //   // Загружаем изображение, чтобы проверить его доступность
+  //   const img = new Image();
+
+  //   img.src = user.photo_url;
+  //   img.onload = function() {
+  //     userPhotoElement.src = user.photo_url;
+  //     userPhotoElement.style.display = 'block';
+  //   }
+  //   img.onerror = function() {
+  //     console.error('Не удалось загрузить изображение пользователя.');
+  //   }
+  // }
 } else {
   console.error('Не удалось получить данные пользователя.');
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  const navItems = document.querySelectorAll('.footer-nav__item');
+
+  navItems.forEach(item => {
+    item.addEventListener('click', function() {
+      // Удаляем класс is-active у всех элементов
+      navItems.forEach(i => i.classList.remove('is-active'));
+      
+      // Добавляем класс is-active к текущему элементу
+      this.classList.add('is-active');
+    });
+  });
+});
